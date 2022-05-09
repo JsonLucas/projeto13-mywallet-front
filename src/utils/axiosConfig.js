7516f8792b667bb50958987b9dee2b7b27da0d61
Jason <jasonlucas443@gmail.com>
2022-05-09 17:51:23 -0300

    link do back-end modificado

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-const axiosConfig = axios.create({baseURL: 'http://127.0.0.1:5000'});
+const axiosConfig = axios.create({baseURL: 'https://projeto13-mywallet-back-jason.herokuapp.com'});
 export default axiosConfig;
